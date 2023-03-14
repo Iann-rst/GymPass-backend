cfde13b142cd55af45645151e00618e526c55a88
Iann Rodrigues <iannrst11@gmail.com>
2023-03-14 17:09:10 -0300

    feat: Configurando ESLint

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,13 +1,13 @@
-import 'dotenv/config';
+import 'dotenv/config'
 
-import { z } from 'zod';
+import { z } from 'zod'
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
   PORT: z.coerce.number().default(3333),
 })
 
-const _env = envSchema.safeParse(process.env);
+const _env = envSchema.safeParse(process.env)
 
 if (_env.success === false) {
   console.error('❌ Invalid environment variables', _env.error.format())
