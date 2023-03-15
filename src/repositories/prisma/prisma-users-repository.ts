d9fe66feafb071e2d40a5444b468a46f2ac31776
Iann Rodrigues <iannrst11@gmail.com>
2023-03-15 11:56:15 -0300

    feat: Repository Users usando prisma

---
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -0,0 +1,22 @@
+import { prisma } from '@/lib/prisma'
+import { Prisma, User } from '@prisma/client'
+import { UsersRepository } from '../users-repository'
+
+export class PrismaUsersRepository implements UsersRepository {
+  async findUserByEmail(email: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    return user;
+  }
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = await prisma.user.create({
+      data,
+    })
+    return user
+  }
+}
