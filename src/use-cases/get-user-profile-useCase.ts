d94e1ff6103bc076d457b1b80fcb0fb146916217
Iann Rodrigues <iannrst11@gmail.com>
2023-03-15 19:27:59 -0300

    feat: Caso de uso de perfil

---
+++ src/use-cases/get-user-profile-useCase.ts
@@ -0,0 +1,27 @@
+import { UsersRepository } from '@/repositories/users-repository'
+import { User } from '@prisma/client'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+interface GetUserProfileUseCaseRequest {
+  userId: string
+}
+
+interface GetUserProfileUseCaseResponse {
+  user: User
+}
+
+export class GetUserProfileUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    userId,
+  }: GetUserProfileUseCaseRequest): Promise<GetUserProfileUseCaseResponse> {
+    const user = await this.usersRepository.findById(userId)
+
+    if (!user) {
+      throw new ResourceNotFoundError()
+    }
+
+    return { user }
+  }
+}
