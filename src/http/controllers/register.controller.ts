8a8947ed4abcb424c8c7dc51ddc412f26dce10d6
Iann Rodrigues <iannrst11@gmail.com>
2023-03-15 11:30:20 -0300

    feat: Controller para registrar um usuário

---
+++ src/http/controllers/register.controller.ts
@@ -0,0 +1,28 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+import { registerUseCase } from '@/use-cases/register.useCase'
+
+export async function registerController(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  try {
+    await registerUseCase({
+      name,
+      email,
+      password,
+    })
+  } catch (err) {
+    return reply.status(409).send()
+  }
+
+  return reply.status(201).send()
+}
