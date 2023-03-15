a4a02a086b4fec19eee05b5ca23c5676dea16112
Iann Rodrigues <iannrst11@gmail.com>
2023-03-15 15:00:52 -0300

    feat: Adc retorno no registro de usuário

--- src/use-cases/register.useCase.ts
+++ src/use-cases/register.useCase.ts
@@ -1,11 +1,16 @@
-import { UserAlreadyExistsError } from './errors/user-already-exists-error';
-import { UsersRepository } from './../repositories/users-repository';
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+import { UsersRepository } from './../repositories/users-repository'
 import { hash } from 'bcryptjs'
+import { User } from '@prisma/client'
 
 interface RegisterUseCaseRequest {
   name: string
   email: string
   password: string
+}
+
+interface RegisterUseCaseResponse {
+  user: User
 }
 
 // SOLID
@@ -15,19 +20,25 @@ interface RegisterUseCaseRequest {
 export class RegisterUseCase {
   constructor(private usersRepository: UsersRepository) { }
 
-  async execute({ name, email, password }: RegisterUseCaseRequest) {
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
     const password_hash = await hash(password, 6)
 
     const userWithSameEmail = await this.usersRepository.findUserByEmail(email)
 
     if (userWithSameEmail) {
-      throw new UserAlreadyExistsError();
+      throw new UserAlreadyExistsError()
     }
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash,
     })
+
+    return { user }
   }
 }
