b01bb628eb6dec6c0b8c4351a9a57bdb15d5b101
Iann Rodrigues <iannrst11@gmail.com>
2023-03-15 18:13:31 -0300

    feat: Teste do caso de uso de Autenticação

---
+++ src/use-cases/authenticate.spec.ts
@@ -0,0 +1,55 @@
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { hash } from 'bcryptjs'
+import { describe, expect, it } from 'vitest'
+import { AuthenticateUseCase } from './authenticate.useCase'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
+
+describe('Authenticate Use Case', () => {
+  it('should be able to authenticate', async () => {
+    const userRepository = new InMemoryUsersRepository()
+    const authenticateUseCase = new AuthenticateUseCase(userRepository)
+
+    await userRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password_hash: await hash('123456', 6),
+    })
+
+    const { user } = await authenticateUseCase.execute({
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to authenticate with wrong email', async () => {
+    const userRepository = new InMemoryUsersRepository()
+    const authenticateUseCase = new AuthenticateUseCase(userRepository)
+
+    await expect(() =>
+      authenticateUseCase.execute({
+        email: 'johndoe@example.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError)
+  })
+
+  it('should not be able to authenticate with wrong password', async () => {
+    const userRepository = new InMemoryUsersRepository()
+    const authenticateUseCase = new AuthenticateUseCase(userRepository)
+
+    await userRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password_hash: await hash('123456', 6),
+    })
+
+    await expect(() =>
+      authenticateUseCase.execute({
+        email: 'johndoe@example.com',
+        password: '123123',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError)
+  })
+})
