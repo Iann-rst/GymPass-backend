a02bfba0e4da301b42926e5b7f15516dcae59057
Iann Rodrigues <iannrst11@gmail.com>
2023-03-15 19:25:36 -0300

    feat: Buscar usuário pelo id

--- src/repositories/users-repository.ts
+++ src/repositories/users-repository.ts
@@ -3,4 +3,5 @@ import { Prisma, User } from '@prisma/client'
 export interface UsersRepository {
   create(data: Prisma.UserCreateInput): Promise<User>
   findUserByEmail(email: string): Promise<User | null>
+  findById(userId: string): Promise<User | null>
 }
