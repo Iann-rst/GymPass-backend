4c1126df58f8e7edb050c5169c2efe4ef4d3f595
Iann Rodrigues <iannrst11@gmail.com>
2023-03-15 20:11:41 -0300

    feat: Caso de uso de check-in

---
+++ src/use-cases/check-in.ts
@@ -0,0 +1,27 @@
+import { CheckIn } from '@prisma/client'
+import { CheckInsRepository } from '../repositories/check-ins-repository'
+
+interface CheckInUseCaseRequest {
+  userId: string
+  gymId: string
+}
+
+interface CheckInUseCaseResponse {
+  checkIn: CheckIn
+}
+
+export class CheckInUseCase {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    userId,
+    gymId,
+  }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+    const checkIn = await this.checkInsRepository.create({
+      gym_id: gymId,
+      user_id: userId,
+    })
+
+    return { checkIn }
+  }
+}
