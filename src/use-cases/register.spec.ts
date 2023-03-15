3f64d7b1d48a9178fadf451d89417f8119bb80ff
Iann Rodrigues <iannrst11@gmail.com>
2023-03-15 15:10:07 -0300

    fix: await expect

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -49,7 +49,7 @@ describe('Register Use Case', () => {
       password: '123456',
     })
 
-    expect(() =>
+    await expect(() =>
       registerUseCase.execute({
         name: 'John Doe',
         email,
