6ea95b5aa56eb270ccdd5ac44ec7ed5eb903c885
Iann Rodrigues <iannrst11@gmail.com>
2023-03-15 18:12:44 -0300

    feat: Adc rota para autenticação

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from 'fastify'
+import { authenticateController } from './controllers/authenticate.controller'
 import { registerController } from './controllers/register.controller'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', registerController)
+  app.post('/sessions', authenticateController)
 }
