0211342afd4604984b70b338c7ef94b64df710ad
Iann Rodrigues <iannrst11@gmail.com>
2023-03-15 15:01:18 -0300

    feat: Repository in Memory (Teste Unitários)

---
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,30 @@
+import { Prisma, User } from '@prisma/client'
+import { UsersRepository } from '../users-repository'
+
+export class InMemoryUsersRepository implements UsersRepository {
+  public items: User[] = []
+
+  async findUserByEmail(email: string) {
+    const user = this.items.find((user) => user.email === email)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = {
+      id: 'user-1',
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      created_at: new Date(),
+    }
+
+    this.items.push(user)
+
+    return user
+  }
+}
