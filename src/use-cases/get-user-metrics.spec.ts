dcd95fc1d0f454a431b69eb349344f65d12fffa1
Iann Rodrigues <iannrst11@gmail.com>
2023-03-18 15:42:18 -0300

    feat: Use case de buscar o nº de check-ins do User

---
+++ src/use-cases/get-user-metrics.spec.ts
@@ -0,0 +1,30 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { InMemoryCheckInsRepository } from './../repositories/in-memory/in-memory-check-ins-repository'
+import { GetUserMetricsUseCase } from './get-user-metrics'
+
+let checkInsRepository: InMemoryCheckInsRepository
+let getUserMetrics: GetUserMetricsUseCase
+
+describe('Get User Metrics Use Case', () => {
+  beforeEach(() => {
+    checkInsRepository = new InMemoryCheckInsRepository()
+    getUserMetrics = new GetUserMetricsUseCase(checkInsRepository)
+  })
+
+  it('should be able to get check-ins', async () => {
+    await checkInsRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
+    })
+
+    await checkInsRepository.create({
+      gym_id: 'gym-02',
+      user_id: 'user-01',
+    })
+
+    const { checkInsCount } = await getUserMetrics.execute({
+      userId: 'user-01',
+    })
+    expect(checkInsCount).toEqual(2)
+  })
+})
