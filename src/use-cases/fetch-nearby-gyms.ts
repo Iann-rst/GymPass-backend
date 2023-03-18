c96aa9c20ec1ba495a9e58846269e1ca64181281
Iann Rodrigues <iannrst11@gmail.com>
2023-03-18 16:35:31 -0300

    feat: Use case para buscar academias próximas

---
+++ src/use-cases/fetch-nearby-gyms.ts
@@ -0,0 +1,27 @@
+import { Gym } from '@prisma/client'
+import { GymsRepository } from './../repositories/gyms-repository'
+
+interface FetchNearbyGymsUseCaseRequest {
+  userLatitude: number
+  userLongitude: number
+}
+
+interface FetchNearbyGymsUseCaseResponse {
+  gyms: Gym[]
+}
+
+export class FetchNearbyGymsUseCase {
+  constructor(private gymsRepository: GymsRepository) {}
+
+  async execute({
+    userLatitude,
+    userLongitude,
+  }: FetchNearbyGymsUseCaseRequest): Promise<FetchNearbyGymsUseCaseResponse> {
+    const gyms = await this.gymsRepository.findManyNearby({
+      latitude: userLatitude,
+      longitude: userLongitude,
+    })
+
+    return { gyms }
+  }
+}
