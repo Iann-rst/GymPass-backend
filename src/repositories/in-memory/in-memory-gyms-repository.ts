deae9d6174de8a0901747f8a9b12c7a0ccf8da11
Iann Rodrigues <iannrst11@gmail.com>
2023-03-18 14:17:23 -0300

    feat: Implementando método para criar academia

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,8 +1,24 @@
-import { Gym } from '@prisma/client'
+import { Gym, Prisma } from '@prisma/client'
+import { randomUUID } from 'node:crypto'
 import { GymsRepository } from './../gyms-repository'
 
 export class InMemoryGymsRepository implements GymsRepository {
   public items: Gym[] = []
+
+  async create(data: Prisma.GymCreateInput) {
+    const gym = {
+      id: data.id ?? randomUUID(),
+      title: data.title,
+      description: data.description ?? null,
+      phone: data.phone ?? null,
+      latitude: new Prisma.Decimal(data.latitude.toString()),
+      longitude: new Prisma.Decimal(data.longitude.toString()),
+    }
+
+    this.items.push(gym)
+
+    return gym
+  }
 
   async findById(gymId: string): Promise<Gym | null> {
     const gym = this.items.find((gym) => gym.id === gymId)
