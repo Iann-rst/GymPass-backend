5d701b4a21424f392c05a03b44ac8a16cc40bf84
Iann Rodrigues <iannrst11@gmail.com>
2023-03-18 19:45:49 -0300

    feat: Registrando o fastifyJWT na aplicação

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import fastifyJwt from '@fastify/jwt'
 import fastify from 'fastify'
 import { ZodError } from 'zod'
 import { env } from './env'
@@ -5,6 +6,9 @@ import { appRoutes } from './http/routes'
 
 export const app = fastify()
 
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+})
 app.register(appRoutes)
 
 app.setErrorHandler((error, _, reply) => {
