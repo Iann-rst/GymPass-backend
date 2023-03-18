88f2c2d648e186d887d0f21b697490f8b46dceb5
Iann Rodrigues <iannrst11@gmail.com>
2023-03-18 15:22:56 -0300

    feat: método para buscar o histórico de check-ins

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -26,6 +26,12 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     return checkInOnSameDay
   }
 
+  async findManyByUserId(userId: string, page: number) {
+    return this.items
+      .filter((check) => check.user_id === userId)
+      .slice((page - 1) * 20, page * 20)
+  }
+
   async create(data: Prisma.CheckInUncheckedCreateInput) {
     const checkIn = {
       id: randomUUID(),
