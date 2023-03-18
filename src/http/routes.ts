e655939d9407db6b0d7946536ac7816ec054d77e
Iann Rodrigues <iannrst11@gmail.com>
2023-03-18 20:14:12 -0300

    feat: Rota de profile do usuário

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,13 @@
 import { FastifyInstance } from 'fastify'
-import { authenticateController } from './controllers/authenticate.controller'
-import { registerController } from './controllers/register.controller'
+import { authenticateController } from './controllers/authenticate-controller'
+import { profileController } from './controllers/profile-controller'
+import { registerController } from './controllers/register-controller'
+import { verifyJWT } from './middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', registerController)
   app.post('/sessions', authenticateController)
+
+  /** Authenticated */
+  app.get('/me', { onRequest: [verifyJWT] }, profileController)
 }
