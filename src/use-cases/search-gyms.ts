83c5970d14bab79b0d9065e4a3299a34b7adfa37
Iann Rodrigues <iannrst11@gmail.com>
2023-03-18 16:03:53 -0300

    feat: Caso de uso para buscar academia pelo nome

---
+++ src/use-cases/search-gyms.ts
@@ -0,0 +1,24 @@
+import { Gym } from '@prisma/client'
+import { GymsRepository } from './../repositories/gyms-repository'
+
+interface SearchGymsUseCaseRequest {
+  query: string
+  page: number
+}
+
+interface SearchGymsUseCaseResponse {
+  gyms: Gym[]
+}
+
+export class SearchGymsUseCase {
+  constructor(private gymsRepository: GymsRepository) {}
+
+  async execute({
+    query,
+    page,
+  }: SearchGymsUseCaseRequest): Promise<SearchGymsUseCaseResponse> {
+    const gyms = await this.gymsRepository.searchMany(query, page)
+
+    return { gyms }
+  }
+}
