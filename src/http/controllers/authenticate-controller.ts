eb3a236cc12422eaf7e72a33ad62d5dcbd3593de
Iann Rodrigues <iannrst11@gmail.com>
2023-03-18 20:09:15 -0300

    feat: Implementando JWT no Fastify

--- src/http/controllers/authenticate-controller.ts
+++ src/http/controllers/authenticate-controller.ts
@@ -17,9 +17,22 @@ export async function authenticateController(
   try {
     const authenticateUseCase = makeAuthenticateUseCase()
 
-    await authenticateUseCase.execute({
+    const { user } = await authenticateUseCase.execute({
       email,
       password,
+    })
+
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      },
+    )
+
+    return reply.status(200).send({
+      token,
     })
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
@@ -29,6 +42,4 @@ export async function authenticateController(
     }
     throw err
   }
-
-  return reply.status(200).send()
 }
