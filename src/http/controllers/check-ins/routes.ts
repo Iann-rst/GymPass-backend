8ccd3d8954bc33e4f7b008e6d855297aed02cdee
Iann Rodrigues <iannrst11@gmail.com>
2023-03-19 15:55:50 -0300

    feat: Criando as rotas de check-ins

---
+++ src/http/controllers/check-ins/routes.ts
@@ -0,0 +1,16 @@
+import { FastifyInstance } from 'fastify'
+import { verifyJWT } from '../../middlewares/verify-jwt'
+import { create } from './create'
+import { history } from './history'
+import { metrics } from './metrics'
+import { validate } from './validate'
+
+export async function checkInsRoutes(app: FastifyInstance) {
+  app.addHook('onRequest', verifyJWT)
+
+  app.post('/gyms/:gymId/check-ins', create)
+  app.patch('/check-ins/:checkInId/validade', validate)
+
+  app.get('/check-ins/history', history)
+  app.get('/check-ins/metrics', metrics)
+}
