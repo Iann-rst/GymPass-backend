4b0eb4450eb17d7ce782c0792c94147421a43774
Iann Rodrigues <iannrst11@gmail.com>
2023-03-19 18:40:12 -0300

    feat: Rota para refresh token

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -2,11 +2,15 @@ import { FastifyInstance } from 'fastify'
 import { verifyJWT } from '../../middlewares/verify-jwt'
 import { authenticateController } from './authenticate-controller'
 import { profileController } from './profile-controller'
+import { refresh } from './refresh'
 import { registerController } from './register-controller'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', registerController)
   app.post('/sessions', authenticateController)
+
+  /* Refresh token */
+  app.patch('/token/refresh', refresh)
 
   /** Authenticated */
   app.get('/me', { onRequest: [verifyJWT] }, profileController)
