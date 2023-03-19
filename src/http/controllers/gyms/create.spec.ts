d162b899181239e868bc79458d34a703f581e988
Iann Rodrigues <iannrst11@gmail.com>
2023-03-19 16:35:03 -0300

    test: Teste E2E Criar academia

---
+++ src/http/controllers/gyms/create.spec.ts
@@ -0,0 +1,31 @@
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { app } from '../../../app'
+
+describe('Create Gym Controller (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a gym', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const response = await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'JavaScript Gym',
+        description: 'Some description',
+        phone: '7799999999',
+        latitude: -12.156271,
+        longitude: -44.9875447,
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
