7efee2f252c32355a55235c640109001d3a5f84e
Iann Rodrigues <iannrst11@gmail.com>
2023-03-19 15:55:14 -0300

    feat: Controller pegar a quantidade de checkIns

---
+++ src/http/controllers/check-ins/metrics.ts
@@ -0,0 +1,14 @@
+import { makeGetUserMetricsUseCase } from '@/use-cases/factories/make-get-user-metrics-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+export async function metrics(request: FastifyRequest, reply: FastifyReply) {
+  const getUserMetricsUseCase = makeGetUserMetricsUseCase()
+
+  const { checkInsCount } = await getUserMetricsUseCase.execute({
+    userId: request.user.sub,
+  })
+
+  return reply.status(200).send({
+    checkInsCount,
+  })
+}
