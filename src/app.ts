edd42a57e871c6fdf16472ac26fedbff4bf5c5f2
Iann Rodrigues <iannrst11@gmail.com>
2023-03-19 14:19:37 -0300

    refactor: Rotas da aplicação

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,17 @@ import fastifyJwt from '@fastify/jwt'
 import fastify from 'fastify'
 import { ZodError } from 'zod'
 import { env } from './env'
-import { appRoutes } from './http/routes'
+import { gymsRoutes } from './http/controllers/gyms/routes'
+import { usersRoutes } from './http/controllers/users/routes'
 
 export const app = fastify()
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
 })
-app.register(appRoutes)
+
+app.register(usersRoutes)
+app.register(gymsRoutes)
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
