af0b7328fc0afd60238845b0001774c1dc6a8463
Iann Rodrigues <iannrst11@gmail.com>
2023-03-19 13:57:50 -0300

    test: Teste E2E do registro de usuário

--- src/http/controllers/register-controller.spec.ts
+++ src/http/controllers/register-controller.spec.ts
@@ -1,3 +1,23 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { app } from './../../app'
 
-test('Register Controller', () => {})
+describe('Register Controller (e2e', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'john@doe.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
