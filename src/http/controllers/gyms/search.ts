aa897125c148a11d988bde62d7ccc4ecbb9efb3a
Iann Rodrigues <iannrst11@gmail.com>
2023-03-19 15:12:43 -0300

    feat: Controller para buscar academias

---
+++ src/http/controllers/gyms/search.ts
@@ -0,0 +1,23 @@
+import { makeSearchGymsUseCase } from '@/use-cases/factories/make-search-gyms-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function search(request: FastifyRequest, reply: FastifyReply) {
+  const searchGymsQuerySchema = z.object({
+    query: z.string(),
+    page: z.coerce.number().min(1).default(1),
+  })
+
+  const { query, page } = searchGymsQuerySchema.parse(request.body)
+
+  const searchGymsUseCase = makeSearchGymsUseCase()
+
+  const { gyms } = await searchGymsUseCase.execute({
+    query,
+    page,
+  })
+
+  return reply.status(200).send({
+    gyms,
+  })
+}
