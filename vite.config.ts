41d9ca8f553fac48c013cc87154c4981a9256aa8
Iann Rodrigues <iannrst11@gmail.com>
2023-03-19 13:03:46 -0300

    feat: Criando test environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
-import { defineConfig } from 'vitest/config'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    environmentMatchGlobs: [['src/http/controllers/**', 'prisma']],
+  },
 })
